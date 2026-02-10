d5a27ca81b550adcd1d41e2d4729410b5b4c72b2
Ishant <ishantawasthi350@gmail.com>
2026-02-10 12:49:43 +0530

    React without vite

---
+++ 01react/src/Chai.js
@@ -0,0 +1,16 @@
+
+function Chai(){
+
+return (
+
+    <>
+
+        <h2> React without vite and   Chai component  </h2>
+         <h2>  Chai component you can import in index.js and App.js both </h2>
+
+         </>
+)
+
+}
+
+export  default Chai
