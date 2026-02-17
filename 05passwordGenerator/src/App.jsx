0a3d73c1e3b5ade96d9147311a8142e8c108ba59
Ishant <ishantawasthi350@gmail.com>
2026-02-17 23:55:55 +0530

    custom  hook ---> currency Converter

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
       
 
            useEffect(()=>{
-        generatePassword()
+      //  generatePassword()
       },[ length,numberAllowed,characterAllowed,generatePassword])
 
 
